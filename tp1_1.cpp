f8012afd8cb0397df16bb973f61dfbbb1cac08fa
Gimenez Francisco Miguel <franciscomguelgimenez708@gmail.com>
2024-04-01 01:10:16 -0300

    p3

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main(){
     cout << "hola mundo";
+
+    int a=78, *b = &a;
+    cout << endl << *b << endl << &b << endl << b << endl << &a << endl << sizeof(a);
     return 0;
 }
